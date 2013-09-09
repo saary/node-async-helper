7c546f9e5552d83b960f778e181cca6a619410a1
Saar Yahalom <saary@microsoft.com>
2013-09-09 22:01:46 +0300

    fix spaces/tabs

--- node-async.h
+++ node-async.h
@@ -5,80 +5,80 @@
 
 namespace NodeUtils
 {
-	using namespace v8;
+    using namespace v8;
 
-	template<typename TInput, typename TResult> 
-	class Async 
-	{
-	public:
-		struct Baton {
-			int error_code;
-			std::string error_message;
+    template<typename TInput, typename TResult> 
+    class Async 
+    {
+    public:
+        struct Baton {
+            int error_code;
+            std::string error_message;
 
-			// Custom data
-			std::shared_ptr<TInput> data;
-			std::shared_ptr<TResult> result;
+            // Custom data
+            std::shared_ptr<TInput> data;
+            std::shared_ptr<TResult> result;
 
-		private:
-			uv_work_t request;
-			std::function<void (Baton*)> doWork;
-			std::function<void (Handle<Function> callback, Baton*)> afterWork;
-			Persistent<Function> callback;
+        private:
+            uv_work_t request;
+            std::function<void (Baton*)> doWork;
+            std::function<void (Handle<Function> callback, Baton*)> afterWork;
+            Persistent<Function> callback;
 
-		friend Async<TInput, TResult>;
-		};
+            friend Async<TInput, TResult>;
+        };
 
-		static void Run(
-			std::shared_ptr<TInput> input, 
-			std::function<void (Baton*)> doWork, 
-			std::function<void (Handle<Function> callback, Baton*)> 
-			afterWork, Handle<Function> callback)
-		{
-			HandleScope scope;
+        static void Run(
+            std::shared_ptr<TInput> input, 
+            std::function<void (Baton*)> doWork, 
+            std::function<void (Handle<Function> callback, Baton*)> 
+            afterWork, Handle<Function> callback)
+        {
+            HandleScope scope;
 
-			Baton* baton = new Baton();
-			baton->request.data = baton;
-			baton->callback = Persistent<Function>::New(callback);
-			baton->error_code = 0;
-			baton->data = input;
-			baton->doWork = doWork;
-			baton->afterWork = afterWork;
+            Baton* baton = new Baton();
+            baton->request.data = baton;
+            baton->callback = Persistent<Function>::New(callback);
+            baton->error_code = 0;
+            baton->data = input;
+            baton->doWork = doWork;
+            baton->afterWork = afterWork;
 
-			uv_queue_work(uv_default_loop(), &baton->request, AsyncWork, AsyncAfter);      
-		}
+            uv_queue_work(uv_default_loop(), &baton->request, AsyncWork, AsyncAfter);      
+        }
 
-	private:
-		static void AsyncWork(uv_work_t* req) 
-		{
-			// No HandleScope!
+    private:
+        static void AsyncWork(uv_work_t* req) 
+        {
+            // No HandleScope!
 
-			Baton* baton = static_cast<Baton*>(req->data);
+            Baton* baton = static_cast<Baton*>(req->data);
 
-			// Do work in threadpool here.
-			// Set baton->error_code/message on failures.
-			// Set baton->result with a final result object
-			baton->doWork(baton);
-		}
+            // Do work in threadpool here.
+            // Set baton->error_code/message on failures.
+            // Set baton->result with a final result object
+            baton->doWork(baton);
+        }
 
-		static void AsyncAfter(uv_work_t* req, int status) 
-		{
-			HandleScope scope;
-			Baton* baton = static_cast<Baton*>(req->data);
+        static void AsyncAfter(uv_work_t* req, int status) 
+        {
+            HandleScope scope;
+            Baton* baton = static_cast<Baton*>(req->data);
 
-			// typical AfterWorkFunc implementation
-			//if (baton->error) 
-			//{
-			//    // Call callback with error object.
-			//} 
-			//else 
-			//{
-			//    // Call callback with results.
-			//}
+            // typical AfterWorkFunc implementation
+            //if (baton->error) 
+            //{
+            //    // Call callback with error object.
+            //} 
+            //else 
+            //{
+            //    // Call callback with results.
+            //}
 
-			baton->afterWork(baton->callback, baton);
+            baton->afterWork(baton->callback, baton);
 
-			baton->callback.Dispose();
-			delete baton;
-		}
-	};
+            baton->callback.Dispose();
+            delete baton;
+        }
+    };
 }
