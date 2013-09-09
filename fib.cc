cf08c36bbdb91fabc97bb64599e1163192ae11a2
Saar Yahalom <saary@microsoft.com>
2013-09-09 21:51:23 +0300

    fix spaces/tabs

--- fib.cc
+++ fib.cc
@@ -7,52 +7,53 @@ using namespace NodeUtils;
 
 Handle<Value> DoFib(const Arguments& args) 
 {
-	HandleScope scope;
+    HandleScope scope;
 
-	if (args.Length() > 0) 
-	{
-		std::shared_ptr<int> num(new int(args[0]->Int32Value()));
-		Async<int, int>::Run(num,
-			[] (Async<int, int>::Baton* baton) 
-			{
-				auto target = *baton->data;
-				auto prev = 0;
-				auto curr = 1;
-				for(int i=0; i<target; i++)
-				{
-					int tmp = curr;
-					curr = curr + prev;
-					prev = tmp;
-				}
+    if (args.Length() > 0) 
+    {
+        std::shared_ptr<int> num(new int(args[0]->Int32Value()));
+        Async<int, int>::Run(
+            num,
+            [] (Async<int, int>::Baton* baton) 
+            {
+                auto target = *baton->data;
+                auto prev = 0;
+                auto curr = 1;
+                for(int i=0; i<target; i++)
+                {
+                    int tmp = curr;
+                    curr = curr + prev;
+                    prev = tmp;
+                }
 
-				baton->result = std::shared_ptr<int>(new int(curr));
-			},
-			[] (Handle<Function> callback, Async<int, int>::Baton* baton) 
-			{
-				HandleScope scope;
+                baton->result = std::shared_ptr<int>(new int(curr));
+            },
+            [] (Handle<Function> callback, Async<int, int>::Baton* baton) 
+            {
+                HandleScope scope;
 
 
-				if (baton->error_code)
-				{
-					Local<Value> err = Exception::Error(String::New(baton->error_message.c_str()));
-					Local<Value> argv[] = { err };
-					callback->Call(Context::GetCurrent()->Global(), _countof(argv), argv);
-				}
-				else
-				{
-					Local<Value> argv[] = { Local<Value>::New(Undefined()), Integer::New(*baton->result) };
-					callback->Call(Context::GetCurrent()->Global(), _countof(argv), argv);
-				}
-			},
-			args[1].As<Function>());
-	}
+                if (baton->error_code)
+                {
+                    Local<Value> err = Exception::Error(String::New(baton->error_message.c_str()));
+                    Local<Value> argv[] = { err };
+                    callback->Call(Context::GetCurrent()->Global(), _countof(argv), argv);
+                }
+                else
+                {
+                    Local<Value> argv[] = { Local<Value>::New(Undefined()), Integer::New(*baton->result) };
+                    callback->Call(Context::GetCurrent()->Global(), _countof(argv), argv);
+                }
+            },
+            args[1].As<Function>());
+    }
 
-	return scope.Close(Undefined());
+    return scope.Close(Undefined());
 }
 
 void init(Handle<Object> exports) {
-	exports->Set(String::NewSymbol("run"),
-		FunctionTemplate::New(DoFib)->GetFunction());
+    exports->Set(String::NewSymbol("run"),
+        FunctionTemplate::New(DoFib)->GetFunction());
 }
 
 NODE_MODULE(fib, init)
